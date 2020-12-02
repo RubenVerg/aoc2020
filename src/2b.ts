522ec378d105bc36609dbb861df795a6b05d2e33
Ruben Vergani <RubenVerg@outlook.com>
2020-12-02 10:02:45 +0100

    small tweak

--- src/2b.ts
+++ src/2b.ts
@@ -1,12 +1,12 @@
 import e from './tools/main';
 
 export default e(async _input => {
-	const RE = /(\d+)-(\d+) (\w): (\w+)/;
+	const RE = /(\d+)-(\d+) (\w):( \w+)/;
 
 	return _input.split(/\r?\n/).reduce((valid, line) => {
 		const [_, pa, pb, letter, pw] = RE.exec(line);
 
-		if (Number(pw[+pa - 1] === letter) ^ Number(pw[+pb - 1] === letter)) valid++;
+		if (Number(pw[pa] === letter) ^ Number(pw[pb] === letter)) valid++;
 
 		return valid;
 	}, 0);
