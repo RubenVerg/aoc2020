9b90dc0d993faa087840d9e5f630a9ad7b211efe
Ruben Vergani <RubenVerg@outlook.com>
2020-12-02 08:54:35 +0100

    days 1 and 2

--- src/1a.ts
+++ src/1a.ts
@@ -1,5 +1,9 @@
 import e from './tools/main';
+import { Combination as _Combination } from 'js-combinatorics/commonjs/combinatorics';
+import type _CombTypes from 'js-combinatorics';
+
+const Combination = _Combination as typeof _CombTypes.Combination;
 
 export default e(async _input => {
-	return null;
+	return (new Combination(_input.split(/\r?\n/).map(i => +i), 2).toArray() as [number, number][]).find(([a, b]) => a + b === 2020).reduce((a, b) => a * b);
 });
--- src/2b.ts
+++ src/2b.ts
@@ -1,5 +1,13 @@
 import e from './tools/main';
 
 export default e(async _input => {
-	return null;
+	const RE = /(\d+)-(\d+) (\w): (\w+)/;
+
+	return _input.split(/\r?\n/).reduce((valid, line) => {
+		const [_, pa, pb, letter, pw] = RE.exec(line);
+
+		if (Number(pw[+pa - 1] === letter) ^ Number(pw[+pb - 1] === letter)) valid++;
+
+		return valid;
+	}, 0);
 });
