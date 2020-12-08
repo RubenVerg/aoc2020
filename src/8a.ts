c1a525f7985b2974b9c5548938977132686d7d6b
Ruben Vergani <RubenVerg@outlook.com>
2020-12-08 09:24:15 +0100

    day 8

--- src/8a.ts
+++ src/8a.ts
@@ -1,5 +1,33 @@
 import e from './tools/main';
 
+type InstructionName = 'nop' | 'acc' | 'jmp';
+type Instruction = [InstructionName, number];
+type Runnable = [Instruction, boolean];
+
 export default e(async _input => {
-	return null;
+	const insts: Runnable[] = [];
+	for (let inst of _input.split(/\n/)) {
+		const match = (/(nop|acc|jmp) ([+-]\d+)/).exec(inst);
+		insts.push([[match[1] as InstructionName, +match[2]], false]);
+	}
+	let acc = 0;
+	let current = 0;
+	while (insts[current][1] !== true) {
+		switch(insts[current][0][0]) {
+			case 'acc':
+				acc += insts[current][0][1];
+				insts[current][1] = true;
+				current++;
+				break;
+			case 'nop':
+				insts[current][1] = true;
+				current++;
+				break;
+			case 'jmp':
+				insts[current][1] = true;
+				current += insts[current][0][1];
+				break;
+		}
+	}
+	return acc;
 });
--- src/8b.ts
+++ src/8b.ts
@@ -1,5 +1,67 @@
 import e from './tools/main';
 
+type InstructionName = 'nop' | 'acc' | 'jmp';
+type Instruction = [InstructionName, number];
+type Runnable = [Instruction, boolean];
+
 export default e(async _input => {
-	return null;
+	const original: Runnable[] = [];
+	for (let inst of _input.split(/\n/)) {
+		const match = (/(nop|acc|jmp) ([+-]\d+)/).exec(inst);
+		original.push([[match[1] as InstructionName, +match[2]], false]);
+	}
+	const worlds: Runnable[][] = [];
+	for (let i = 0; i < original.length; i++) {
+		if (original[i][0][0] === 'acc') continue;
+		if (original[i][0][0] === 'jmp') {
+			worlds.push(
+				original
+					.slice()
+					.map(i => i.slice() as Runnable)
+					.map(i => [i[0].slice(), i[1]] as Runnable)
+					.map((_, ind) => ind === i ? [['nop', _[0][1]], _[1]] as Runnable : _)
+			)
+		}
+		if (original[i][0][0] === 'nop') {
+			worlds.push(
+				original
+					.slice()
+					.map(i => i.slice() as Runnable)
+					.map(i => [i[0].slice(), i[1]] as Runnable)
+					.map((_, ind) => ind === i ? [['jmp', _[0][1]], _[1]] as Runnable : _)
+			)
+		}
+	}
+	let toBeReturned = 0;
+	for (let world of worlds) {
+		let acc = 0;
+		let current = 0;
+		let toBreak = false;
+		while (world[current][1] !== true) {
+			switch (world[current][0][0]) {
+				case 'acc':
+					acc += world[current][0][1];
+					world[current][1] = true;
+					current++;
+					break;
+				case 'nop':
+					world[current][1] = true;
+					current++;
+					break;
+				case 'jmp':
+					world[current][1] = true;
+					current += world[current][0][1];
+					break;
+			}
+			if (current === world.length) {
+				toBreak = true;
+				break;
+			}
+		}
+		if (toBreak) {
+			toBeReturned = acc;
+			break;
+		}
+	}
+	return toBeReturned;
 });
